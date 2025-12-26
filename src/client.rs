08287d4fe7526e2f381062f12d15f34d81376fbc
Ngakan Gandhi <ngandhi@pmintl.net>
2025-12-27 05:23:33 +0700

    feat: move some functions as struct methods

--- src/client.rs
+++ src/client.rs
@@ -31,7 +31,7 @@ impl AwsClient {
 }
 
 impl AthenaService {
-    pub async fn list_data_catalogs(&self) -> Result<Vec<String>, ShellError> {
+    pub async fn list_catalogs(&self) -> Result<Vec<String>, ShellError> {
         let mut catalogs: Vec<String> = Vec::new();
         let mut next_token: Option<String> = None;
 
@@ -58,118 +58,117 @@ impl AthenaService {
         }
         Ok(catalogs)
     }
-}
 
-pub async fn list_databases(
-    client: &AthenaClient,
-    catalog_name: &str,
-) -> Result<Vec<String>, ShellError> {
-    let mut databases: Vec<String> = Vec::new();
-    let mut response = client
-        .list_databases()
-        .catalog_name(catalog_name)
-        .into_paginator()
-        .send();
-    while let Some(stream) = response.next().await {
-        match stream {
-            Ok(x) => {
-                for db in x.database_list() {
-                    let name = db.name();
-                    databases.push(name.into())
+    pub async fn list_databases(&self, catalog_name: &str) -> Result<Vec<String>, ShellError> {
+        let mut databases: Vec<String> = Vec::new();
+        let mut response = self
+            .0
+            .list_databases()
+            .catalog_name(catalog_name)
+            .into_paginator()
+            .send();
+        while let Some(stream) = response.next().await {
+            match stream {
+                Ok(x) => {
+                    for db in x.database_list() {
+                        let name = db.name();
+                        databases.push(name.into())
+                    }
+                }
+                Err(e) => {
+                    println!("{:#?}", e);
                 }
             }
-            Err(e) => {
-                println!("{:#?}", e);
-            }
         }
+        Ok(databases)
     }
-    Ok(databases)
-}
 
-// pub async fn invoke_query(
-//     client: &AthenaClient,
-//     db_name: &str,
-//     query: &str,
-//     output_bucket: &str,
-// ) -> Result<String, AthenaError> {
-//     let response = client
-//         .start_query_execution()
-//         .query_execution_context(QueryExecutionContext::builder().database(db_name).build())
-//         .query_string(query)
-//         .result_configuration(
-//             ResultConfiguration::builder()
-//                 .output_location(format!("s3://{output_bucket}/"))
-//                 .build(),
-//         )
-//         .send()
-//         .await?;
-//     let query_execution_id = response.query_execution_id().unwrap();
-//     Ok(query_execution_id.to_string())
-// }
+    // pub async fn invoke_query(
+    //     client: &AthenaClient,
+    //     db_name: &str,
+    //     query: &str,
+    //     output_bucket: &str,
+    // ) -> Result<String, AthenaError> {
+    //     let response = client
+    //         .start_query_execution()
+    //         .query_execution_context(QueryExecutionContext::builder().database(db_name).build())
+    //         .query_string(query)
+    //         .result_configuration(
+    //             ResultConfiguration::builder()
+    //                 .output_location(format!("s3://{output_bucket}/"))
+    //                 .build(),
+    //         )
+    //         .send()
+    //         .await?;
+    //     let query_execution_id = response.query_execution_id().unwrap();
+    //     Ok(query_execution_id.to_string())
+    // }
 
-// pub async fn has_query_succeeded(
-//     client: &AthenaClient,
-//     execution_id: &str,
-//     attempt: i32,
-//     timeout: u64,
-// ) -> Result<bool> {
-//     for _ in 0..RETRY_MAX_ATTEMPTS {
-//         let response = client
-//             .get_query_execution()
-//             .send()
-//             .await
-//             .context("Failed to get query execution status")?;
+    // pub async fn has_query_succeeded(
+    //     client: &AthenaClient,
+    //     execution_id: &str,
+    //     attempt: i32,
+    //     timeout: u64,
+    // ) -> Result<bool> {
+    //     for _ in 0..RETRY_MAX_ATTEMPTS {
+    //         let response = client
+    //             .get_query_execution()
+    //             .send()
+    //             .await
+    //             .context("Failed to get query execution status")?;
 
-//         let execution = response
-//             .query_execution()
-//             .context("Missing query execution data")?;
+    //         let execution = response
+    //             .query_execution()
+    //             .context("Missing query execution data")?;
 
-//         let state = execution
-//             .status()
-//             .and_then(|s| s.state())
-//             .context("Missing query state")?;
+    //         let state = execution
+    //             .status()
+    //             .and_then(|s| s.state())
+    //             .context("Missing query state")?;
 
-//         match state.as_str() {
-//             "SUCCEEDED" => return Ok(true),
-//             "FAILED" | "CANCELLED" => return Ok(false),
-//             "RUNNING" | "QUEUED" => {
-//                 tokio::time::sleep(tokio::time::Duration::from_secs(timeout)).await;
-//             }
-//             _ => return Ok(false),
-//         }
-//     }
-//     Ok(false)
-// }
+    //         match state.as_str() {
+    //             "SUCCEEDED" => return Ok(true),
+    //             "FAILED" | "CANCELLED" => return Ok(false),
+    //             "RUNNING" | "QUEUED" => {
+    //                 tokio::time::sleep(tokio::time::Duration::from_secs(timeout)).await;
+    //             }
+    //             _ => return Ok(false),
+    //         }
+    //     }
+    //     Ok(false)
+    // }
 
-pub async fn get_query_results(
-    client: &AthenaClient,
-    execution_id: &str,
-) -> Result<Vec<Vec<String>>, ShellError> {
-    let mut result_sets: Vec<Vec<String>> = Vec::new();
-    let mut result = client
-        .get_query_results()
-        .query_execution_id(execution_id)
-        .into_paginator()
-        .send();
+    pub async fn get_query_results(
+        &self,
+        execution_id: &str,
+    ) -> Result<Vec<Vec<String>>, ShellError> {
+        let mut result_sets: Vec<Vec<String>> = Vec::new();
+        let mut result = self
+            .0
+            .get_query_results()
+            .query_execution_id(execution_id)
+            .into_paginator()
+            .send();
 
-    while let Some(stream) = result.next().await {
-        match stream {
-            Ok(x) => {
-                let rs = x.result_set().unwrap();
-                rs.rows().iter().for_each(|row| {
-                    let mut row_data: Vec<String> = Vec::new();
-                    row.data().iter().for_each(|data| {
-                        if let Some(d) = data.var_char_value() {
-                            row_data.push(d.to_string());
-                        } else {
-                            row_data.push("".to_string());
-                        }
+        while let Some(stream) = result.next().await {
+            match stream {
+                Ok(x) => {
+                    let rs = x.result_set().unwrap();
+                    rs.rows().iter().for_each(|row| {
+                        let mut row_data: Vec<String> = Vec::new();
+                        row.data().iter().for_each(|data| {
+                            if let Some(d) = data.var_char_value() {
+                                row_data.push(d.to_string());
+                            } else {
+                                row_data.push("".to_string());
+                            }
+                        });
+                        result_sets.push(row_data);
                     });
-                    result_sets.push(row_data);
-                });
+                }
+                Err(e) => println!("{:?}", e),
             }
-            Err(e) => println!("{:?}", e),
         }
+        Ok(result_sets)
     }
-    Ok(result_sets)
 }
