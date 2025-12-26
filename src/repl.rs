3c846d009fc412cbd07f4d477bc5cba5a90000ff
Ngakan Gandhi <ngandhi@pmintl.net>
2025-12-26 19:33:45 +0700

    feat: fix the tokio select! loop logic

--- src/repl.rs
+++ src/repl.rs
@@ -1,24 +1,27 @@
 #![allow(unused)]
 use crate::error::Result;
 use aws_sdk_athena::Client as AthenaClient;
-use std::io;
 use std::io::Write;
+use std::io::{self, BufReader};
 use std::sync::mpsc;
 use std::thread;
+use tokio::io::{AsyncBufReadExt, AsyncReadExt};
+use tokio::io::{BufReader as TokioBufReader, Stdin};
 
 pub struct Repl {
-    prompt: String,        // prompt chars
-    input_buf: String,     // buffer to accumulate stdin input
-    line_buf: String,      // holds complete command for processing
+    prompt: String, // prompt chars
+    line_buf: String,
+    input_buf: Vec<String>, // buffer to accumulate stdin input
+    // holds complete command for processing
     is_in_multiline: bool, // state management of the input
 }
 
 impl Repl {
     pub fn new(profile: &str) -> Self {
         Repl {
-            prompt: String::from(profile),
-            input_buf: String::new(),
+            prompt: format!("{}> ", profile),
             line_buf: String::new(),
+            input_buf: Vec::new(),
             is_in_multiline: false,
         }
     }
@@ -37,9 +40,18 @@ Type 'exit;' to quit
         )
     }
 
+    pub async fn read_line(&self) -> Result<String> {
+        // Create the reader
+        let mut reader = tokio::io::BufReader::new(tokio::io::stdin());
+        let mut buffer = Vec::new();
+        let _fut = reader.read_until(b'\n', &mut buffer).await;
+        Ok(String::from_utf8(buffer)?)
+    }
+
     pub async fn repl_loop(&mut self) -> Result<()> {
         // Print header when first time entering the shell
         self.print_header();
+
         // Begin REPL loop
         loop {
             // Read line from stdin and flush immediately to stdout.
@@ -58,31 +70,54 @@ Type 'exit;' to quit
             // Flush the output to ensure the prompt is displayed immediately
             io::stdout().flush().unwrap();
 
-            // Read line from stdin into the input buffer
-            io::stdin().read_line(&mut self.input_buf).unwrap();
-
-            if self.input_buf.contains(";") {
-                // Command complete, move and process
-                self.line_buf = std::mem::take(&mut self.input_buf);
-                self.is_in_multiline = false;
-                // Sanitize the line_buf string logic
-                let command = self
-                    .line_buf
-                    .split_whitespace()
-                    .collect::<Vec<&str>>()
-                    .join(" ")
-                    .replace(" ;", ";");
-                if command == "exit;" {
-                    println!("Exiting Athena Shell!");
-                    return Ok(());
+            // Wait for either stdin or sigint
+            tokio::select! {
+                linebuf = self.read_line() => {
+                    let tmp = linebuf.unwrap();
+                    self.input_buf.push(tmp.clone());
+                    if tmp.contains(";") {
+                        // Command complete, move and process
+                        // self.line_buf = std::mem::take(&mut self.input_buf);
+                        self.is_in_multiline = false;
+                        // Sanitize the line_buf string logic
+                        let command = self
+                            .input_buf
+                            .iter()
+                            .map(|s| s.replace('\n', "").trim().to_string())
+                            .filter(|s| !s.is_empty())
+                            .collect::<Vec<String>>()
+                            .join(" ")
+                            .replace(" ;", ";");
+                        if command == "exit;" {
+                            println!("Exiting Athena Shell!");
+                            return Ok(());
+                        }
+                        // Execute command
+                        println!("{}", command); // placeholder for command execution
+                        // Empty the line buffer
+                        // input buffer should already empty after the take()
+                        self.input_buf.clear();
+                    } else if tmp == "\n" || tmp == "\r\n" { // checks for newline-only input
+                        if !self.input_buf.is_empty() {
+                            self.is_in_multiline = true; // keep printing "|" as prompt if input_buf contains data
+                        } else {
+                            self.input_buf.clear(); // regenerate prompt for newline-only input
+                            self.is_in_multiline = false;
+                        }
+                    } else {
+                        self.is_in_multiline = true;
+                    }
                 }
-                // Execute command
-                println!("{}", command); // placeholder for command execution
-                // Empty the line buffer
-                // input buffer should already empty after the take()
-                self.line_buf.clear();
-            } else {
-                self.is_in_multiline = true
+                _ = tokio::signal::ctrl_c() => {
+                    println!("\nCtrl+C detected, cancelling current input...");
+                    println!("Press ENTER twice to return to the prompt");
+                    self.input_buf.clear();
+                    self.line_buf.clear();
+                    self.is_in_multiline = false;
+                    // consume pending input
+                    let _ = tokio::io::stdin().read(&mut [0u8; 1024]).await;
+                    tokio::time::sleep(tokio::time::Duration::from_millis(250)).await;
+                }
             }
         }
     }
