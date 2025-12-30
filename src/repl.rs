eb3194ade0ad5dee23a1ca523f25cf9ee09b3a77
Ngakan Gandhi <ngandhi@pmintl.net>
2025-12-30 19:28:10 +0700

    refac: use mpsc approach to handle asynchronous stdin and ctrl-c input

--- src/repl.rs
+++ src/repl.rs
@@ -1,14 +1,15 @@
 use crate::aws::error::Result;
 use std::io::Write;
-use std::io::{self};
-use tokio::io::{AsyncBufReadExt, AsyncReadExt};
+use tokio::io::AsyncBufReadExt;
+use tokio::signal;
+use tokio::sync::mpsc;
 
 pub struct Repl {
     prompt: String, // prompt chars
     line_buf: String,
     input_buf: Vec<String>, // buffer to accumulate stdin input
     // holds complete command for processing
-    is_in_multiline: bool, // state management of the input
+    multiline: bool, // state management of the input
 }
 
 impl Repl {
@@ -17,7 +18,7 @@ impl Repl {
             prompt: format!("{}> ", profile),
             line_buf: String::new(),
             input_buf: Vec::new(),
-            is_in_multiline: false,
+            multiline: false,
         }
     }
 
@@ -35,87 +36,97 @@ Type 'exit;' to quit
         )
     }
 
-    pub async fn read_line(&self) -> Result<String> {
-        // Create the reader
-        let mut reader = tokio::io::BufReader::new(tokio::io::stdin());
-        let mut buffer = Vec::new();
-        let _fut = reader.read_until(b'\n', &mut buffer).await;
-        buffer.pop();
-        Ok(String::from_utf8(buffer)?)
-    }
-
     pub async fn repl_loop(&mut self) -> Result<()> {
         // Print header when first time entering the shell
         self.print_header();
 
+        // Create a channel for delivering lines from stdin task
+        let (tx, mut rx) = mpsc::unbounded_channel::<String>();
+
+        // Create a dedicated stdin reader task
+        tokio::spawn(async move {
+            let stdin = tokio::io::stdin();
+            let mut reader = tokio::io::BufReader::new(stdin);
+            let mut line = String::new();
+
+            // Check for stdin input from keyboard
+            // SIGNAL is not treated as a valid stdin input from keyboard
+            loop {
+                line.clear();
+                match reader.read_line(&mut line).await {
+                    Ok(0) => break, // EOF -> 0 bytes read
+                    Ok(_) => {
+                        if tx.send(line.clone()).is_err() {
+                            break;
+                        }
+                    }
+                    Err(_) => break,
+                }
+            }
+        });
+
+        self.multiline = false;
+
         // Begin REPL loop
         loop {
-            // Read line from stdin and flush immediately to stdout.
-            // is_in_multiline is a state management flag which is true when input buffer is not empty and not terminated with a semicolon.
-            // In this case it creates a new line feed prefixed with a pipe "| " char.
-            // If input buffer is terminated with a semicolon, then close the input stream and process the command.
-            // If return char (\n or \r\n) is fed to the buffer, then clear the input buffer.
-            //
+            // self.multiline is a state management flag which is true when input buffer is not empty and not terminated with a semicolon.
             // If the buffer is processing multi-line input, change the prompt into "|"
-            if self.is_in_multiline {
+            if self.multiline {
                 print!("| ");
             } else {
                 print!("{}", self.prompt);
             }
 
             // Flush the output to ensure the prompt is displayed immediately
-            let _ = io::stdout().flush();
-
-            // Spawn a tokio task so we can abort it later in case of SIGINT
-            let read_line_task = self.read_line();
+            let _ = std::io::stdout().flush();
 
             // Wait for either stdin or sigint
             tokio::select! {
-                linebuf = read_line_task => {
-                    let tmp = linebuf?;
-                    self.input_buf.push(tmp.clone());
-                    if tmp.contains(";") {
-                        // Command complete, move and process
-                        // self.line_buf = std::mem::take(&mut self.input_buf);
-                        self.is_in_multiline = false;
-                        // Sanitize the line_buf string logic
-                        let command = self
-                            .input_buf
-                            .iter()
-                            .map(|s| s.replace('\n', "").trim().to_string())
-                            .filter(|s| !s.is_empty())
-                            .collect::<Vec<String>>()
-                            .join(" ")
-                            .replace(" ;", ";");
-                        if command == "exit;" {
-                            println!("Exiting Athena Shell!");
-                            return Ok(());
-                        }
-                        // Execute command
-                        println!("{}", command); // placeholder for command execution
-                        // Empty the line buffer
-                        // input buffer should already empty after the take()
-                        self.input_buf.clear();
-                    } else if tmp == "\n" || tmp == "\r\n" { // checks for newline-only input
-                        if !self.input_buf.is_empty() {
-                            self.is_in_multiline = true; // keep printing "|" as prompt if input_buf contains data
-                        } else {
-                            self.input_buf.clear(); // regenerate prompt for newline-only input
-                            self.is_in_multiline = false;
-                        }
-                    } else {
-                        self.is_in_multiline = true;
+                _ = signal::ctrl_c() => {
+                    if self.multiline {
+                        println!("\n");
+                        self.multiline = false;
+                        self.line_buf.clear();
+                    } else { // Ctrl-C during normal prompt
+                        println!("\n(Use Ctrl-D to exit)");
                     }
                 }
-                _ = tokio::signal::ctrl_c() => {
-                    println!("\nCtrl+C detected, cancelling current input...");
-                    println!("Press ENTER twice to return to the prompt");
-                    self.input_buf.clear();
-                    self.line_buf.clear();
-                    self.is_in_multiline = false;
-                    // consume pending input
-                    let _ = tokio::io::stdin().read(&mut [0u8; 1024]).await;
-                    tokio::time::sleep(tokio::time::Duration::from_millis(250)).await;
+                Some(line) = rx.recv() => {
+                    if !self.multiline {
+                        if line.trim().is_empty() { // handle case where user just press Enter (empty input)
+                            continue;
+                        }
+                        if line.trim_end().ends_with(';') {
+                            let command = line.trim();
+                            if command.to_lowercase() == "exit;" {
+                                println!("Exiting Athena CLI!");
+                                return Ok(());
+                            }
+                            println!("Input command: {}", command); // placeholder for actual work
+                        } else {
+                            self.multiline = true;
+                            self.input_buf.push(line);
+                        }
+                    } else {
+                        self.line_buf.push_str(&line);
+                        if line.trim_end().ends_with(';') {
+                            let command = self
+                                .input_buf
+                                .iter()
+                                .map(|s| s.replace('\n', "").trim().to_string())
+                                .filter(|s| !s.is_empty())
+                                .collect::<Vec<String>>()
+                                .join(" ")
+                                .replace(" ;", ";");
+                            if command.to_lowercase() == "exit;" {
+                                println!("Exiting Athena CLI!");
+                                return Ok(());
+                            }
+                            println!("Multiline command: {}", command); // placeholder for string join
+                            self.multiline = false;
+                            self.input_buf.clear();
+                        }
+                    }
                 }
             }
         }
